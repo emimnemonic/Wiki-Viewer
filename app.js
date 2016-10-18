aba8c027975a299888e3da58f8f10b82a7723793
emi <emi.mnemonic@gmail.com>
2016-10-19 12:48:40 +1300

    Set up for local dev w/o internet #sigh

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+
+    $('#search-btn').click(function () {
+        $(this).fadeToggle("slow", "linear");
+        $('#search-field').fadeToggle("slow", "linear");
+    });
+
+});
