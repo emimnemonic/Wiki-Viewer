4009b300fefaadf73f909b084534caf1e118d4f8
emi <emi.mnemonic@gmail.com>
2016-10-19 16:47:56 +1300

    Fix responsive issues and add jquery functions

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
 $(document).ready(function () {
 
-    $('#search-btn').click(function () {
-        $(this).fadeToggle("slow", "linear");
-        $('#search-field').fadeToggle("slow", "linear");
+    var searchBtn = $('#search-btn'),
+        form = $('#search-form'),
+        field = $('#search-field'),
+        type = $('#type-search'),
+        close = $('#close'),
+        clear = $('#clear-search');
+
+    searchBtn.click(function () {
+        $(this).fadeToggle("slow", "linear", function () {
+            form.fadeToggle("slow", "linear");
+            field.focus();
+        });
     });
 
+    field.keypress(function () {
+        if ($(this).val() !== "") {
+            type.addClass("hide");
+            close.removeClass("hide");
+        }
+    });
+
+    field.keyup(function () {
+        if ($(this).val() === "") {
+            close.addClass("hide");
+            type.removeClass("hide");
+        }
+    });
+
+    clear.click(function () {
+        field.val("");
+        close.addClass("hide");
+        type.removeClass("hide");
+        field.focus();
+    });
 });
